d1c67a4651a674b5f01c10e739936b7883df4eee
1 <2>
2020-07-20 18:25:49 +0200

    heroku vars

--- server/config/config.js
+++ server/config/config.js
@@ -12,11 +12,10 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://root:hUMyLjjGgxtC3Hqw@cluster0.yvogi.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
-//forzar usar la bda de producion
-//urlDB = 'mongodb+srv://root:hUMyLjjGgxtC3Hqw@cluster0.yvogi.mongodb.net/cafe';
+
 
 
 // esta URLDB es inventada
